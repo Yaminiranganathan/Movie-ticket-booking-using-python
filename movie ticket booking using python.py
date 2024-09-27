6ecc3a17afc55a3ca3854ebb1aa76d5c795171df
Yamini R <85726572+Yaminiranganathan@users.noreply.github.com>
2024-09-27 10:52:07 +0530

    Update movie ticket booking using python.py

--- movie ticket booking using python.py
+++ movie ticket booking using python.py
@@ -1,166 +1,214 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
+import random
 
 print('                                WELCOME TO BOOK MY SHOW                              ')
-print(''' Choose you language to book a ticket for the movie:
-      1.Tamil
-      2.English''')
-a=int(input('Enter your language: '))
-if a==1:
-    print('''List of tamil movies availabe now :
-          1.Alaipayuthe
-          2.Ponniyin Selvan
-          3.Ghilli
-          4.Vinnai thandi Varuvaya''')
-    b=int(input('Enter your movie for booking: '))
-    if b==1:
-        print('''The locations available for alaipayuthe movie are:
-              1.Chennai
-              2.Madurai
-              3.Trichy
-              4.kanyakumari''')
-    elif b==2:
-         print('''The locations available for Ponniyin selvan movie are:
-              1.Chennai
-              2.Madurai
-              3.Salem''')
-    elif b==3:
-        print('''The locations available for ghilli movie are:
-              1.Chennai
-              2.Tanjore''')
-    elif b==4:
-        print('''The locations available for alaipayuthe movie are:
-              1.Chennai
-              2.Madurai
-              ''')
-    else:
-        print('Enter a valid location............!!!!!!')
+
+while True:
+    print('''Choose your language to book a ticket for the movie:
+      1. Tamil
+      2. English''')
     
-    c=int(input('Choose your location for booking:'))
-    if c==1 or c==3:
-        print('''List of theatres in chennai to watch alaipayuthe movie:
-                  1.Kamala theatre
-                  2.PVR
-                  3.Inox
-                  4.Rajkamal cinemas''')
-    elif c==2 or c==4:
-        print('''List of theatres in chennai to watch alaipayuthe movie:
-                  1.Vetri theatre
-                  2.PVR
-                  3.Kumaran cinemas ''')
-    else:
-        print('Enter the valid theatre...!!!!!!!')
+    while True:
+        a = int(input('Enter your language: '))
+        if a in [1, 2]:
+            break  # valid input, exit the loop
+        else:
+            print('Invalid input. Please enter 1 for Tamil or 2 for English.')
+
+    if a == 1:
+        print('''List of Tamil movies available now:
+          1. Alaipayuthe
+          2. Ponniyin Selvan
+          3. Ghilli
+          4. Vinnai Thandi Varuvaya''')
         
-    d=int(input('choose your theatre:'))
-    if d==1 or d==3:
-        print('''Show timings available are:
-                      1. 10.a.m
-                      2. 3.a.m
-                      3. 9.p.m''')
-    elif d==2 or d==4 or d==5:
-        print('''Show timings available are:
-                      1. 9.30.a.m
-                      2. 2.30.a.m
-                      3. 7.p.m''')
-    else:
-        print(' enter a valid timing...!!!!')
-    
-    
-    e=int(input('choose your timing:'))
-    if e==1 or e==3:
+        while True:
+            b = int(input('Enter your movie for booking: '))
+            if b in [1, 2, 3, 4]:
+                break
+            else:
+                print('Invalid movie option. Please choose a valid movie.')
+
+        if b == 1:
+            print('''The locations available for Alaipayuthe movie are:
+              1. Chennai
+              2. Madurai
+              3. Trichy
+              4. Kanyakumari''')
+        elif b == 2:
+            print('''The locations available for Ponniyin Selvan movie are:
+              1. Chennai
+              2. Madurai
+              3. Salem''')
+        elif b == 3:
+            print('''The locations available for Ghilli movie are:
+              1. Chennai
+              2. Tanjore''')
+        elif b == 4:
+            print('''The locations available for Vinnai Thandi Varuvaya movie are:
+              1. Chennai
+              2. Madurai''')
+
+        while True:
+            c = int(input('Choose your location for booking: '))
+            if c in [1, 2, 3, 4]:
+                break
+            else:
+                print('Invalid location. Please enter a valid option.')
+
+        if c == 1:
+            print('''List of theatres in Chennai:
+                  1. Kamala Theatre
+                  2. PVR
+                  3. Inox
+                  4. Rajkamal Cinemas''')
+        elif c == 2:
+            print('''List of theatres in Madurai:
+                  1. Vetri Theatre
+                  2. PVR
+                  3. Kumaran Cinemas''')
+        elif c == 3:
+            print('''List of theatres in Trichy:
+                  1. Sangeetha
+                  2. PVR''')
+        elif c == 4:
+            print('''List of theatres in Kanyakumari:
+                  1. Meenakshi Cinemas
+                  2. Sathyam Cinemas''')
+
+        while True:
+            d = int(input('Choose your theatre: '))
+            if d in [1, 2, 3, 4]:
+                break
+            else:
+                print('Invalid theatre option. Please choose a valid theatre.')
+
+        if d in [1, 3]:
+            print('''Show timings available:
+                      1. 10:00 AM
+                      2. 3:00 PM
+                      3. 9:00 PM''')
+        elif d in [2, 4]:
+            print('''Show timings available:
+                      1. 9:30 AM
+                      2. 2:30 PM
+                      3. 7:00 PM''')
+
+        while True:
+            e = int(input('Choose your timing: '))
+            if e in [1, 2, 3]:
+                break
+            else:
+                print('Invalid timing. Please choose a valid timing.')
+
         print('''The ticket price available are:
-                      1.60 rs
-                      2.120 rs
-                      3.190 rs''')
-    elif e==2:
+                      1. 60 Rs
+                      2. 120 Rs
+                      3. 190 Rs''')
+
+        while True:
+            f = int(input('Choose your ticket price: '))
+            if f in [1, 2, 3]:
+                break
+            else:
+                print('Invalid price. Please choose a valid price.')
+
+        print(f'''If you want to confirm the booking, 
+               click '1' to continue
+               click '0' to exit''')
+
+        while True:
+            g = int(input('Enter your choice to confirm: '))
+            if g in [1, 0]:
+                break
+            else:
+                print('Invalid input. Please enter 1 to confirm or 0 to exit.')
+
+        if g == 1:
+            otp = random.randrange(1500, 9999)
+            print(f'The OTP for your ticket booking is: {otp}')
+            print('Your ticket was successfully booked. Thank you for using Book My Show. Enjoy your movie!')
+        elif g == 0:
+            print('OOPS! You exited from the Book My Show movie booking.')
+        break  # exit the loop after booking is done
+
+    elif a == 2:
+        print('''List of English movies available now: 
+          1. Conjuring
+          2. Titanic''')
+
+        while True:
+            b = int(input('Enter your movie for booking: '))
+            if b in [1, 2]:
+                break
+            else:
+                print('Invalid movie option. Please choose a valid movie.')
+
+        if b == 1:
+            print('''The locations available for Conjuring movie are:
+              1. Chennai
+              2. Madurai''')
+        elif b == 2:
+            print('''The locations available for Titanic movie are:
+              1. Chennai
+              2. Trichy''')
+
+        while True:
+            c = int(input('Choose your location for booking: '))
+            if c in [1, 2]:
+                break
+            else:
+                print('Invalid location. Please enter a valid option.')
+
+        print('''List of theatres:
+                  1. Kamala Theatre
+                  2. PVR''')
+
+        while True:
+            d = int(input('Choose your theatre: '))
+            if d in [1, 2]:
+                break
+            else:
+                print('Invalid theatre option. Please choose a valid theatre.')
+
+        print('''Show timings available:
+                      1. 10:00 AM
+                      2. 3:00 PM
+                      3. 9:00 PM''')
+
+        while True:
+            e = int(input('Choose your timing: '))
+            if e in [1, 2, 3]:
+                break
+            else:
+                print('Invalid timing. Please choose a valid timing.')
+
         print('''The ticket price available are:
-                      1.110 rs
-                      2.190 rs''')
-    else:
-        print('Enter valid price:')
-    
+                      1. 60 Rs
+                      2. 120 Rs
+                      3. 190 Rs''')
 
-    f=int(input('Choose your ticket price: '))
+        while True:
+            f = int(input('Choose your ticket price: '))
+            if f in [1, 2, 3]:
+                break
+            else:
+                print('Invalid price. Please choose a valid price.')
 
+        print(f'''If you want to confirm the booking, 
+               click '1' to continue
+               click '0' to exit''')
 
-    print(f'''
-                          
-        If you want to confirm the booking , 
-        click '1' for continue
-        click '0' for exit''')
-    g=int(input('Enter your choice to confirm: '))
-    if g==1:
-        import random
-        otp=random.randrange(1500,9999)
-        print('The OTP for your ticket booking is:',otp)
-        print('Your ticket was successfully booked. Thank you for using bookmyshow. Enjoy your movie..!!!!')
-    elif g==0:
-        print('OOPS! You exit from the bookmyshow movie booking')
-    
-                    
-            
-if a==2:
-    print('''List of tamil movies availabe now : 
-    1.Conjuring
-    2.Titanic''')
-    b=int(input('Enter your movie for booking: '))
-    if b==1:
-        print('''The locations available for alaipayuthe movie are:
-              1.Chennai
-              2.Madurai''')
-    elif b==2:
-        print('''The locations available for alaipayuthe movie are:
-              1.Chennai
-              2.Trichy''')
-    c=int(input('Choose your location for booking:')) 
-    if c==1 or c==2:
-        print('''List of theatres in chennai to watch alaipayuthe movie:
-                  1.Kamala theatre
-                  2.PVR''')
-    else:
-        print('Enter the valid theatre...!!!!!!!')
-    
-    d=int(input('choose your theatre:'))
-    if d==1 or d==2:
-        print('''Show timings available are:
-                      1. 10.a.m
-                      2. 3.a.m
-                      3. 9.p.m''')
-    else:
-        print(' enter a valid timing...!!!!')
-    
-    e=int(input('choose your timing:'))
-    if e==1 or e==2 or e==3:
-        print('''The ticket price available are:
-                      1.60 rs
-                      2.120 rs
-                      3.190 rs''')
-    else:
-        print('Enter valid price:')
-    
+        while True:
+            g = int(input('Enter your choice to confirm: '))
+            if g in [1, 0]:
+                break
+            else:
+                print('Invalid input. Please enter 1 to confirm or 0 to exit.')
 
-    f=int(input('Choose your ticket price:'))
-
-
-    print(f'''If you want to confirm the booking , 
-           click '1' for continue
-           click '0' for exit''')
-    g=int(input('Enter your choice to confirm: '))
-    if g==1:
-        import random
-        otp=random.randrange(1500,9999)
-        print(f'''The OTP for your ticket booking is:'{otp}
-               Your ticket was successfully booked. Thank you for using bookmyshow. Enjoy your movie..!!!!''')
-    elif g==0:
-        print('OOPS! You exit from the bookmyshow movie booking')
-
-            
-
-
-
-
-
-
-
+        if g == 1:
+            otp = random.randrange(1500, 9999)
+            print(f'The OTP for your ticket booking is: {otp}')
+            print('Your ticket was successfully booked. Thank you for using Book My Show. Enjoy your movie!')
+        elif g == 0:
+            print('OOPS! You exited from the Book My Show movie booking.')
+        break  # exit the loop after booking is done
